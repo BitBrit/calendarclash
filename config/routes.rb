0ce75dd929be86c1dbe3ee65e9508863937f5fd4
Rachelle Keblitis <rkeblitis@gmail.com>
2014-11-15 15:01:04 -0800

    adds a static_controller and home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Calendarclash::Application.routes.draw do
+
+  # Static Contoller
+  root "static_pages#home"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
